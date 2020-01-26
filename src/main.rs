03c61a4f9dd4da657409e7b3a1ab0db24cc9c468
red <red.avtovo@gmail.com>
2020-01-26 15:50:04 +0100

    fixed binding address

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ async fn main() -> std::io::Result<()> {
             .service(web::resource("/auth/refresh").route(web::post().to(token::refresh)))
             .service(web::resource("/auth/check").route(web::post().to(token::check)))
     )
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
